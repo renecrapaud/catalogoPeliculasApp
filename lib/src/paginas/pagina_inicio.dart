db558432fd5ec2c77c418b25565a0ed8945061f4
crapaud <renecrapaud@gmail.com>
2021-03-03 23:34:43 -0600

    Llamada http y obtención de datos del api de peliculas

--- lib/src/paginas/pagina_inicio.dart
+++ lib/src/paginas/pagina_inicio.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:catalogo_peliculas_app/src/widgets/swiper_personalizado.dart';
+import 'package:catalogo_peliculas_app/src/providers/proveedor_peliculas.dart';
 
 class PaginaInicio extends StatelessWidget {
   @override
@@ -27,6 +28,9 @@ class PaginaInicio extends StatelessWidget {
   }
 
   Widget _swiperTarjetas() {
+
+    final proveedorPeliculas = new ProveedorPeliculas();
+    proveedorPeliculas.obtEnCines();
     return CardSwiper(
       listaElems: [1,2,3,4,5]
     );
---
+++ lib/src/providers/proveedor_peliculas.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:catalogo_peliculas_app/src/modelos/modelo_pelicula.dart';
+
+class ProveedorPeliculas{
+  String _apiKey = '29f50e4e760003c8bbc958bb061fe2a6';
+  String _url = 'api.themoviedb.org';
+  String _idioma = 'es-MX';
+
+  Future<List<Pelicula>> obtEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing', {
+      'api_key' : _apiKey,
+      'language' : _idioma
+    });
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+    final peliculas = new Peliculas.fromJsonList(decodedData['results']);
+    return peliculas.items;
+  }
+}
